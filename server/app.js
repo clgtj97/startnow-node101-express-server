9f2a2c89e29f4750bca0ae826c361376b900f8bc
clgtj91 <clgjt97@gmail.com>
2018-07-27 13:42:48 -0700

    cool cat's just where born this way

---
+++ server/app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var morgan = require("morgan");
+var topSpots = require("./data.json");
+
+var app = express();
+
+app.use(morgan("dev"));
+
+app.get('/', function(req, res) {
+   res.send("");
+});
+
+app.get("/data", function(req,res) {
+   res.send(topSpots);
+});
+
+module.exports = app;
+
+graph.facebook.com/cesarlf?fields = first_name
